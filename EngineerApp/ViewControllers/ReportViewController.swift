54ca005369cbf6ab894e1a5dd848c47b28960511
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-07-09 17:30:41 +0900

    viewWillAppearのメソッド

--- EngineerApp/ViewControllers/ReportViewController.swift
+++ EngineerApp/ViewControllers/ReportViewController.swift
@@ -14,38 +14,42 @@ final class ReportViewController: UIViewController {
     @IBOutlet private weak var totalHourLabel: UILabel!
     @IBOutlet private weak var totalMinuteLabel: UILabel!
     
-    var hour = 0
-    var minute = 0
-    
     var results: [ReportData] = []
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.hour = 0
-        self.minute = 0
-        //resultsにreportDataの配列を代入
-        let realm = try! Realm()
-        let results = realm.objects(ReportData.self)
-        
-        for content in results {
-            let totalHour = Int(content.hour)
-            self.hour += totalHour
-            let totalMinute = Int(content.minute)
-            self.minute += totalMinute
-        }
-        //minuteが60を超えてしまった場合に、超えた分をhourに加えて、残りをminuteとする
-        if minute >= 59 {
-            self.hour += minute / 60
-            self.minute = minute % 60
-        }
-        self.totalHourLabel.text = "\(hour)"
-        self.totalMinuteLabel.text = "\(minute)"
+        totalCalculate()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(hexString: "FEFFDE")
         navigationItem.title = "合計勉強時間"
+    }
+    
+    func totalCalculate() {
+        var hour: Int = 0
+        var minute: Int = 0
+        
+        //resultsにreportDataの配列を代入
+        let realm = try! Realm()
+        let results = realm.objects(ReportData.self)
+        
+        for content in results {
+            let totalHour = content.hour
+            hour += totalHour
+            let totalMinute = Int(content.minute)
+            minute += totalMinute
+        }
+        
+        //minuteが60を超えてしまった場合に、超えた分をhourに加えて、残りをminuteとする
+        if minute >= 59 {
+            hour += minute / 60
+            minute = minute % 60
+        }
+        
+        self.totalHourLabel.text = "\(hour)"
+        self.totalMinuteLabel.text = "\(minute)"
     }
 }
 
