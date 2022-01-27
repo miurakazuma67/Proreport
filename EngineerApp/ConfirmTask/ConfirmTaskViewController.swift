883fda09927597e621ac938f0cf8931da39fe10b
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2022-01-27 22:41:58 +0900

    tabbar

--- EngineerApp/ConfirmTask/ConfirmTaskViewController.swift
+++ EngineerApp/ConfirmTask/ConfirmTaskViewController.swift
@@ -9,25 +9,25 @@ import UIKit
 
 final class ConfirmTaskViewController: UIViewController {
     
-    @IBOutlet private weak var addTaskView: AddTaskView!
     //Model定義
     var taskData = TaskData()
+    //taskDataを格納する配列を作る
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        AddTaskView.addTaskDelegate = self
-        addTaskView.taskTableView.register(TaskTableViewCell.nib, forCellReuseIdentifier: TaskTableViewCell.identifier)
-        addTaskView.taskTableView.delegate = self
-        addTaskView.taskTableView.dataSource = self
-        createCircle()
-        addTaskView.plusButton.backgroundColor = Colors.PopGreen
-        addTaskView.plusButton.tintColor = Colors.TextLightGrayColor
+//        addTaskView.taskTableView.register(TaskTableViewCell.nib, forCellReuseIdentifier: TaskTableViewCell.identifier)
+//        addTaskView.taskTableView.delegate = self
+//        addTaskView.taskTableView.dataSource = self
+//        createCircle()
+//        addTaskView.plusButton.backgroundColor = Colors.PopGreen
+//        addTaskView.plusButton.tintColor = Colors.TextLightGrayColor
     }
     
     func createCircle() {
-        addTaskView.plusButton.layer.masksToBounds = true
-        addTaskView.plusButton.layer.cornerRadius =
-            addTaskView.plusButton.bounds.width / 2
+//        addTaskView.plusButton.layer.masksToBounds = true
+//        addTaskView.plusButton.layer.cornerRadius =
+//            addTaskView.plusButton.bounds.width / 2
     }
     
 }
@@ -46,12 +46,11 @@ extension ConfirmTaskViewController: UITableViewDataSource, UITableViewDelegate 
 
 //ボタンタップ時の画面遷移をdelegateで実装した
 //TODO: 10/9 修正する
-extension ConfirmTaskViewController: AddTaskDelegate {
-    func addTask() {
-        let taskStoryboard: UIStoryboard = UIStoryboard(name: "EditTask", bundle: nil)
-        let task = taskStoryboard.instantiateViewController(withIdentifier: "EditTask") as! EditTaskViewController
-        let nav = UINavigationController(rootViewController: task)
-        self.present(nav, animated: true, completion: nil)
-        
-    }
-}
+//extension ConfirmTaskViewController: AddTaskDelegate {
+//    func addTask() {
+//        let taskStoryboard: UIStoryboard = UIStoryboard(name: "EditTask", bundle: nil)
+//        let task = taskStoryboard.instantiateViewController(withIdentifier: "EditTask") as! EditTaskViewController
+//        let nav = UINavigationController(rootViewController: task)
+//        self.present(nav, animated: true, completion: nil)
+//    }
+//}
--- EngineerApp/Designs/TabBarController.swift
+++ EngineerApp/Designs/TabBarController.swift
@@ -10,12 +10,24 @@ import UIKit
 final class TabBarController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tabBar.tintColor = Colors.MainGreen
-        self.tabBar.barTintColor = Colors.PopGreen
+        setTabbarColor()
         self.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
            super.viewDidAppear(animated)
        }
+    
+    ///tabbarの色変更
+    private func setTabbarColor() {
+        self.tabBar.tintColor = Colors.MainGreen
+        self.tabBar.barTintColor = Colors.PopGreen
+        
+        //ios15対応
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.backgroundColor = Colors.PopGreen
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
 }
